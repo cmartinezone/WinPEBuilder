58d1f7d19f37e7afd4c020eac8ca61fd2272355e
Carlos Martinez <carlos0492@outlook.com>
2019-01-15 11:27:12 -0500

    adding feautes

--- WinPEBuilder.cmd
+++ WinPEBuilder.cmd
@@ -1,9 +1,12 @@
-@echo off
- 
+@ECHO OFF
+
+:: Developed by: Carlos Martinez @cmartinezone Date: 1/15/2019
+:: WinPEBuilder 1.0 Auto Generate WinPE ISO with drivers and script incorporate
+
 REM Where to put Windows PE tree and ISO
 Set winpe_root=%~dp0WinPE-Root
  
-REM ADK installation path. ADK 10 can be found here: https://www.microsoft.com/en-US/download/details.aspx?id=39982
+REM ADK installation path. ADK 10 can be found here: https://docs.microsoft.com/en-us/windows-hardware/get-started/adk-install
 Set adk_path=%programfiles(x86)%\Windows Kits\10\Assessment and Deployment Kit
  
 REM Drivers tree path
@@ -11,7 +14,8 @@ Set drivers_path=%~dp0Add-Drivers
 
 REM User scripts location to include in new WinPE image. Scripts tree should include the modified startnet.cmd in which you can add your stuff
 Set scripts_path=%~dp0Add-Scripts
- 
+
+REM Directory path for finall iso generated  
 set ISO_Path=%~dp0WinPE-ISO
  
 REM Calling a script which sets some useful variables 
@@ -26,14 +30,14 @@ call copype.cmd amd64 %winpe_root%
 REM Mounting WinPE wim-image
 Dism /Mount-Wim /WimFile:%winpe_root%\media\sources\boot.wim /index:1 /MountDir:%winpe_root%\mount
  
-REM Adding some useful packages. Packages description and dependencies for WinPE 8.1 can be found here: http://technet.microsoft.com/en-us/library/hh824926.aspx
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-HTA.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-Scripting.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-WMI.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-NetFx.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-PowerShell.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-DismCmdlets.cab"
-rem Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-StorageWMI.cab"
+REM Adding some useful packages. Packages description and dependencies for WinPE 10 can be found here: http://technet.microsoft.com/en-us/library/hh824926.aspx
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-HTA.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-Scripting.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-WMI.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-NetFx.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-PowerShell.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-DismCmdlets.cab"
+Dism /image:%winpe_root%\mount /Add-Package /PackagePath:"%adk_path%\Windows Preinstallation Environment\amd64\WinPE_OCs\WinPE-StorageWMI.cab"
  
 REM Adding drivers
 Dism /image:%winpe_root%\mount /Add-Driver /driver:%drivers_path% /recurse
@@ -50,7 +54,5 @@ Dism /Unmount-Wim /MountDir:%winpe_root%\mount\ /Commit
 REM Creationg ISO image from WinPE tree
 Makewinpemedia /iso /f %winpe_root% %ISO_Path%\winpe_amd64.iso
 
-REM open winpe directory 
-explorer %ISO_Path%
+pause
 
-pause 
